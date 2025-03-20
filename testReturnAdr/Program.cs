38fd2e836f83124b842bb666ee0fa894215fda52
Kurt Pernstich <kurt.pernstich@zhaw.ch>
2025-03-20 10:40:33 +0100

    changed to AddMicrosoftIdentityWebApp

--- testReturnAdr/Program.cs
+++ testReturnAdr/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
 using testReturnAdr.Components;
 using testReturnAdr.Components.Account;
 using testReturnAdr.Data;
@@ -21,19 +22,22 @@ builder.Services.AddAuthentication(options =>
         options.DefaultScheme = IdentityConstants.ApplicationScheme;
         options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
     })
-    .AddMicrosoftAccount(microsoftOptions =>
-    {
-        microsoftOptions.ClientId = builder.Configuration["Authentication:Microsoft:ClientId"] ?? throw new InvalidOperationException("Authentication:Microsoft:ClientId is not configured.");
-        microsoftOptions.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"] ?? throw new InvalidOperationException("Authentication:Microsoft:ClientSecret is not configured.");
-        var tenantId = builder.Configuration["Authentication:Microsoft:TenantId"] ?? throw new InvalidOperationException("Authentication:Microsoft:TenantId is not configured.");
-        microsoftOptions.AuthorizationEndpoint = $"https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/authorize";
-        microsoftOptions.TokenEndpoint = $"https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/token";
-        //microsoftOptions.CallbackPath = new PathString("/signin-microsoft");
-        //microsoftOptions.CallbackPath = new PathString("/signin-oidc");
-        //microsoftOptions.UserInformationEndpoint = $"https://graph.microsoft.com/oidc/userinfo";
-        //microsoftOptions.SaveTokens = true;
-    })
-    .AddIdentityCookies();
+    //.AddMicrosoftAccount(microsoftOptions =>
+    //{
+    //    microsoftOptions.ClientId = builder.Configuration["Authentication:Microsoft:ClientId"] ?? throw new InvalidOperationException("Authentication:Microsoft:ClientId is not configured.");
+    //    microsoftOptions.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"] ?? throw new InvalidOperationException("Authentication:Microsoft:ClientSecret is not configured.");
+    //    var tenantId = builder.Configuration["Authentication:Microsoft:TenantId"] ?? throw new InvalidOperationException("Authentication:Microsoft:TenantId is not configured.");
+    //    microsoftOptions.AuthorizationEndpoint = $"https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/authorize";
+    //    microsoftOptions.TokenEndpoint = $"https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/token";
+    //    //microsoftOptions.CallbackPath = new PathString("/signin-microsoft");
+    //    //microsoftOptions.CallbackPath = new PathString("/signin-oidc");
+    //    //microsoftOptions.UserInformationEndpoint = $"https://graph.microsoft.com/oidc/userinfo";
+    //    //microsoftOptions.SaveTokens = true;
+    //})
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+    // came with the tamplate, but does not work with AddMicrosoftIdentityApp
+    //.AddIdentityCookies();
+    builder.Services.AddAuthentication().AddIdentityCookies();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
